0ef89ff709f153de52e10a05dda81961ec51984c
Firmin MAKABI <codeurlibremakabi@gmail.com>
2022-08-24 14:26:43 +0100

    ajout de la premiere activité

--- app/src/main/java/cd/maksociety/bureaudechange/MainActivity.kt
+++ app/src/main/java/cd/maksociety/bureaudechange/MainActivity.kt
@@ -2,10 +2,24 @@ package cd.maksociety.bureaudechange
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var taux_du_jour = 2030
+        val text_taux = findViewById<TextView>(R.id.taux_jour)
+        val button_next_page = findViewById<Button>(R.id.valid_button)
+
+
+        text_taux.text = "1 dollars vaut $taux_du_jour FC"
+        button_next_page.setOnClickListener {
+            finish()
+        }
+
     }
 }
---
+++ app/src/main/java/cd/maksociety/bureaudechange/lebureau.kt
@@ -0,0 +1,11 @@
+package cd.maksociety.bureaudechange
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class lebureau : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lebureau)
+    }
+}
